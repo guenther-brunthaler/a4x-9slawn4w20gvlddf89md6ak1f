70c49de2bfd6c96a8c5eb5f5f09cd92065225ee7
Guenther Brunthaler <gb@xduo.emgenxx69lwyn5ctlr4nl64ul.local>
2020-01-26 10:11:46 +0100

    Add maintainer script for updating dependencies

---
+++ mkdepend.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+# Maintainer helper script for updating project dependencies. Not needed for
+# just building the project.
+#
+# Version 2020.26
+# Copyright (c) 2020 Guenther Brunthaler. All rights reserved.
+#
+# This script is free software.
+# Distribution is permitted under the terms of the GPLv3.
+
+set -e
+trap 'test $? = 0 || echo "\"$0\" failed!"' 0
+
+println() {
+	printf '%s\n' "$*"
+}
+
+for src in *.c
+do
+	obj=${src%.*}.o
+	rm -f -- "$obj"
+	make -s CPPFLAGS="$CPPFLAGS -MM" "$obj"
+	cat < "$obj"
+done \
+| while read line
+do
+	println "$line" # Unfold continuation lines.
+done | LC_COLLATE=C sort \
+| {
+	set -f
+	while read line
+	do
+		set -- $line
+		echo
+		println "$1 \\"; shift
+		for dep
+		do
+			println " $dep \\"
+		done \
+		| LC_COLLATE=C sort
+	done
+}
